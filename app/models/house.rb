33ab8452c6621a36368340afeb2a5551c1a2270f
Paul Wells <paulwells12@gmail.com>
2013-11-10 14:16:47 -0500

    rough validations

--- app/models/house.rb
+++ app/models/house.rb
@@ -2,5 +2,11 @@ class House < ActiveRecord::Base
  
   has_many :residencies 
   has_many :users, through: :residencies
- 
+  validates :house_name, presence: true
+  validates :longitude, presence: true
+  validates :latitude, presence: true
+  validates :phone_number, length: {is: 10}, numericality: true
+  
+  
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
   has_many :residencies
   has_many :houses, through: :residencies
   
+  validates :phone_number, presence: true, numericality: true
+  validates :name, presence: true
+  
 end
