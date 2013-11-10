b82e2cf32b07d58be79ef2233df0b4e6b00bae4b
Paul Wells <paulwells12@gmail.com>
2013-11-09 23:13:48 -0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HomePhone::Application.config.secret_token = '44f199de7fd2cb392ab3ed7dfe80a791559ef2a57e9eeaaa99934399134c80c3e015a1d2b09dc84b5adfc7a34dc22ada70e82eae73a71c642d440c4cbeb28db6'
