aad3c29acb603e4205ea35cdb3e3f771200075cb
Paul Wells <paulwells12@gmail.com>
2013-11-19 23:33:51 -0500

    changed some relations

--- app/models/whos_home_request.rb
+++ app/models/whos_home_request.rb
@@ -2,6 +2,5 @@ class WhosHomeRequest < ActiveRecord::Base
 	belongs_to :user
 	has_many :whos_home_responses
 
-	attr_accessor :user_id
 	attr_accessor :num_recipients
 end
--- app/models/whos_home_response.rb
+++ app/models/whos_home_response.rb
@@ -1,7 +1,5 @@
 class WhosHomeResponse < ActiveRecord::Base
 	belongs_to :whos_home_request
 
-	attr_accessor :whos_home_request_id
-	attr_accessor :user_id
 	attr_accessor :is_home
 end
